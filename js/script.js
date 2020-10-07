903d7be818999ad26c444f9c98daf8f1c6277a63
dportales <59981221+dportales@users.noreply.github.com>
2020-10-06 22:41:17 -0500

    fixing javascript text replacement

--- js/script.js
+++ js/script.js
@@ -1,6 +1,18 @@
 function init(){
-	var input = document.getElementById('name');
-	var button = documnet.getElementById('submit');
+	var na = document.getElementById('name');
+	var em = document.getElementById('mail');
+	var ps = document.getElementById('pass');
 	
+	var no = document.getElementById('nameout');
+	var eo = document.getElementById('mailout');
+	var po = document.getElementById('passout');
 	
+	var button = document.getElementById('entrybutton');
+	
+	button.addEventListener('click' , function(){
+		no.innerHTML = na.value;
+		eo.innerHTML = em.value;
+		po.innerHTML = ps.value;
+	});
 }
+window.addEventListener('load', init)
