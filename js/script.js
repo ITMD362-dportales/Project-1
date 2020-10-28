9d256e6ccc6ea267a6dd0918dbf4cbb595596403
dportales <59981221+dportales@users.noreply.github.com>
2020-10-28 18:05:53 -0500

    java input verification and conformation complete

--- js/script.js
+++ js/script.js
@@ -1,27 +1,33 @@
-function checkinput(){
 //verify inputs name, email, phone number, and password is filled out
-  var na = document.forms["form"]["name"].value;
-  var em = document.forms["form"]["mail"].value;
-  var pn = document.forms["form"]["phone"].value;
-  var ps = document.forms["form"]["passw"].value;
+function checkinput(){
+  var a = document.forms["test"]["name"].value;
+  var b = document.forms["test"]["mail"].value;
+  var c = document.forms["test"]["phone"].value;
+  var d = document.forms["test"]["passw"].value;
   
-  
+  if (a == "", b == "", c == "", d == ""){
+    alert("All input boxes not filled out")
+  }  
+  else{
+	ver();
+  }
 }
 
+//alert user with prompt to check if what was filled out is correct
 function ver(){
   var na = document.getElementById('name');
   var em = document.getElementById('mail');
   var pn = document.getElementById('phone');
   var ps = document.getElementById('passw');
 	
-  var button = document.getElementById('entrybutton');
-  button.addEventListener('click' , function(){
-	alert("Name: " + na.value  + "\n" +
-	"Email: " + em.value  + "\n" +
-	"Phone Number: " + pn.value  + "\n" +
-	"Password: " + ps.value);
-	
-  });
+  confirm("Name: " + na.value  + "\n" +
+  "Email: " + em.value  + "\n" +
+  "Phone Number: " + pn.value  + "\n" +
+  "Password: " + ps.value + "\n" +
+  "Are these values correct?"
+  );
 }
 
-window.addEventListener('load', ver)
+//wait for check info button to be clicked
+var button = document.getElementById('entrybutton');
+button.addEventListener('click' , checkinput);
