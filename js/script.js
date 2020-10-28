55374128a36e9a152ceed27696a6a3a6a32f61d1
dportales <59981221+dportales@users.noreply.github.com>
2020-10-28 17:02:48 -0500

    rewriting java

--- js/script.js
+++ js/script.js
@@ -1,17 +1,24 @@
+function checkinput(){
+//verify inputs name, email, phone number, and password is filled out
+  var na = document.forms["form"]["name"].value;
+  var em = document.forms["form"]["mail"].value;
+  var pn = document.forms["form"]["phone"].value;
+  var ps = document.forms["form"]["passw"].value;
+  
+  
+}
+
 function init(){
   var na = document.getElementById('name');
   var em = document.getElementById('mail');
+  var pn = document.getElementById('phone');
   var ps = document.getElementById('passw');
-	
-  var no = document.getElementById('nameout');
-  var eo = document.getElementById('emailout');
-  var po = document.getElementById('passout');
 	
   var button = document.getElementById('entrybutton');
   button.addEventListener('click' , function(){
-    no.innerHTML = na.value;
-    eo.innerHTML = em.value;
-    po.innerHTML = ps.value;
+	alert("Name: /n/n/n/n/n");
+	
   });
 }
+
 window.addEventListener('load', init)
