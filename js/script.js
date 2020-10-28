6d2e692739b34e33928c927eb51b261fd70148a2
dportales <59981221+dportales@users.noreply.github.com>
2020-10-28 16:07:13 -0500

    spacing

--- js/script.js
+++ js/script.js
@@ -1,18 +1,17 @@
 function init(){
-	var na = document.getElementById('name');
-	var em = document.getElementById('mail');
-	var ps = document.getElementById('passw');
+  var na = document.getElementById('name');
+  var em = document.getElementById('mail');
+  var ps = document.getElementById('passw');
 	
-	var no = document.getElementById('nameout');
-	var eo = document.getElementById('emailout');
-	var po = document.getElementById('passout');
+  var no = document.getElementById('nameout');
+  var eo = document.getElementById('emailout');
+  var po = document.getElementById('passout');
 	
-	var button = document.getElementById('entrybutton');
-	
-	button.addEventListener('click' , function(){
-		no.innerHTML = na.value;
-		eo.innerHTML = em.value;
-		po.innerHTML = ps.value;
-	});
+  var button = document.getElementById('entrybutton');
+  button.addEventListener('click' , function(){
+    no.innerHTML = na.value;
+    eo.innerHTML = em.value;
+    po.innerHTML = ps.value;
+  });
 }
 window.addEventListener('load', init)
